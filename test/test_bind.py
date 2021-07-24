93c94977eb433cff7b491b43c6e541304f6796f5
jizhi <jizhi@sics.ac.cn>
2021-07-24 14:35:54 +0800

    fix:复用stmt时，没有清理残留的bind数据

---
+++ test/test_bind.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+
+import test_base
+import anchor_python as anchor
+
+class TestCase(test_base.TestBaseCase):
+    def setUp(self):
+        super().setUp()
+        self.cursor.execute("drop table if exists test_bind")
+        self.cursor.execute("create table test_bind(id int , name varchar(256))")
+
+    def test_bind_param(self):
+        cursor = self.connection.cursor()
+        cursor.execute("drop table if exists test_bind_param_heap_1")
+        cursor.execute("create table test_bind_param_heap_1(a int, b double, c int)")
+        self.assertEqual(1, self.cursor.rowcount)
+        cursor.execute("insert into test_bind_param_heap_1 values(:1, :2, 30)",(1,10))
+        cursor.execute("insert into test_bind_param_heap_1 values(:1, :2, 40)",(2,20))
+        self.connection.commit()
+        cursor.execute("delete from test_bind_param_heap_1 where a=:1",(1,))
+        self.connection.commit()
+        cursor.execute("select * from test_bind_param_heap_1")
+        row = cursor.fetchone()
+        data = (2,20.0,40)
+        self.assertEqual(data, row)
+        cursor.execute("update test_bind_param_heap_1 set c=:1", (50,))
+        self.connection.commit()
+        cursor.execute("select * from test_bind_param_heap_1")
+        row = cursor.fetchone()
+        data = (2,20.0,50)
+        self.assertEqual(data, row)
+
+    
+if __name__ == "__main__":
+    test_base.run_test_cases()
