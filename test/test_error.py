48d6d8629ed7d1192ab039c9980c15f7646cb820
heguofeng <heguofeng@sics.ac.cn>
2023-07-14 18:11:20 +0800

    YDBRD-13080:适配python驱动用例，同时支持单机和集群

--- test/test_error.py
+++ test/test_error.py
@@ -9,9 +9,9 @@ class TestCase(test_base.TestBaseCase):
             cursor.execute("drop table test_error_code")
         except Exception as e:
             error = str(e)
-            expectMsg = "[1:12]YAS-02012 table or view does not exist"
-            self.assertEqual(error, expectMsg)
-    
+            expectMsg = "YAS-02012 table or view does not exist"
+            self.assertTrue(expectMsg in error)
+
     def test_bind_error_code(self):
         cursor = self.connection.cursor()
         cursor.execute("drop table if exists test_bind_error")
@@ -22,9 +22,9 @@ class TestCase(test_base.TestBaseCase):
         except Exception as e:
             error = str(e)
             expectMsg = "YAS-00008 type convert error : not a valid number"
-            self.assertEqual(error, expectMsg)
-        
+            self.assertTrue(expectMsg in error)
 
-    
+
+
 if __name__ == "__main__":
     test_base.run_test_cases()
