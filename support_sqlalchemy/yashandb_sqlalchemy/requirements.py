80f342a8aa2810868470fa53a85f25579c438c1f
liuliangjie <liuliangjie@sics.ac.cn>
2024-07-02 15:04:50 +0800

    fix: move sqlalchemy

--- support_sqlalchemy/yashandb_sqlalchemy/requirements.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.testing.requirements import SuiteRequirements
-
-from sqlalchemy.testing import exclusions
-
-class Requirements(SuiteRequirements):
-    @property
-    def empty_inserts_executemany(self):
-        return exclusions.closed()
